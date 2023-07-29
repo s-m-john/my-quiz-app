e0073fdcb3b9d869bf458f23c8d1fcc19e6777e3
Sophia John <smc_john@hotmail.com>
2023-07-29 01:06:54 -0400

    Updated and working perfectly

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// Instead of using ReactDom.render, use createRoot
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
