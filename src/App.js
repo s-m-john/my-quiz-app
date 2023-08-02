51b735ec8104b3b29e109e86299bc19ca2ae5ee2
Sophia John <smc_john@hotmail.com>
2023-08-02 04:07:26 -0400

    Instructions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import QuizInstructions from './components/quiz/QuizInstructions';
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/play/instructions" element={<QuizInstructions />} />
       </Routes>
     </Router>
   );
---
+++ src/components/quiz/QuizInstructions.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+const QuizInstructions = () => (
+    <Fragment>
+        <Helmet><title>Quiz App - Instructions</title></Helmet>
+        <div className="instructions container">
+            <h1>How to Play the Game</h1>
+            <p>Ensure you read this guide from start to finish.</p>
+            <ul className="browser-default" id="main-list">
+                <li>The game has a duration of 15 minutes and ends as soon as your time elapses.</li>
+                <li>Each game consists of 15 questions.</li>
+                <li>
+                    Every question contains 4 options.
+                    {/* <img src={options} alt="Quiz App options example" /> */}
+                </li>
+                <li>
+                    Select the option which best answers the question by clicking (or selecting) it.
+                    {/* <img src={answer} alt="Quiz App answer example" /> */}
+                    </li>
+                <li>Try your best! Part of learning is making mistakes.</li>
+                <li>You can quit the game at any time. Your progress will be saved.</li>
+
+            </ul>
+        </div>
+    </Fragment>
+);
+
+export default QuizInstructions;
