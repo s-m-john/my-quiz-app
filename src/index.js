3c4494f9e9a266265e03543125a828c91a62832c
Sophia John <smc_john@hotmail.com>
2023-08-02 00:31:44 -0400

    button update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import '../node_modules/materialize-css/dist/js/materialize.min.js'
 import './styles/styles.scss';
 import App from './App';
 
