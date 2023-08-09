d97835a32fd90e55d64392aad50deffc1e36a537
Sophia John <smc_john@hotmail.com>
2023-08-09 01:55:58 -0400

    updated summary stats

--- src/components/quiz/QuizSummary.js
+++ src/components/quiz/QuizSummary.js
@@ -1,5 +1,6 @@
 import React, { Component,Fragment } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
 
 class QuizSummary extends Component {
@@ -26,13 +27,74 @@ class QuizSummary extends Component {
     }
     render() {
         //console.log(this.props);
-        const { state } = this.props.location;
-        let stats;
-        if (state !== undefined) {
-            stats = (<h1>Stats are available</h1>);
-            
-        } else (<h1>No Stats available. Please take a quiz.</h1>);
+        const { state, score } = this.props.location;
+        let stats, remark;
 
+        if (score <= 30) {
+            remark = 'You need more practice!';
+        } else if (score > 30 && score <= 50) {
+            remark = 'Better luck next time!';
+        } else if (score <= 70 && score > 50) {
+            remark = 'You can do better!';
+        } else if (score >= 71 && score <= 84) {
+            remark = 'You did great!';
+        } else {
+            remark = 'You\'re an absolute genius!';
+        }
+
+        if (state !== undefined) {
+            stats = (
+                <Fragment>
+                <div>
+                    <span className="mdi mdi-check-circle-outline success-icon"></span>
+                </div>
+                <h1>Quiz has ended</h1>
+                <div className="container stats">
+                    <h4>{remark}</h4>
+                    <h2>Your Score: {this.state.score.toFixed(0)}&#37;</h2>
+                    <span className="stat left">Total number of questions: </span>
+                    <span className="right">{this.state.numberOfQuestions}</span><br />
+
+                    <span className="stat left">Total number of questions: </span>
+                    <span className="right">{this.state.numberOfQuestions}</span><br />
+
+                    <span className="stat left">Total number of questions: </span>
+                    <span className="right">{this.state.numberOfQuestions}</span><br />
+
+                    <span className="stat left">Total number of questions: </span>
+                    <span className="right">{this.state.numberOfQuestions}</span><br />
+
+                    <span className="stat left">Total number of questions: </span>
+                    <span className="right">{this.state.numberOfQuestions}</span>
+                </div>
+                <section>
+                    <ul>
+                        <li>
+                            <Link to="/">Back to Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/play/quiz">Play Again</Link>
+                        </li>
+                    </ul>
+                </section>
+                </Fragment>
+            );
+            
+        } else {
+            stats = (
+                <section>
+                    <h1 className="no-stats">No Statistics Available</h1>
+                    <ul>
+                        <li>
+                            <Link to="/">Back to Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/play/quiz">Take a Quiz</Link>
+                        </li>
+                    </ul>
+                </section>
+            ); 
+        }
         return (
             <Fragment>
                 <Helmet><title>Quiz App - Summary</title></Helmet>
